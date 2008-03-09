44fcd5804bb8dfda24aba27e219b9b8ad49e286e
mr.idcmp <mr.idcmp@8ebe8d7f-f847-0410-ac63-8f6eca9f32e0>
2008-03-09 22:15:30 +0000

    Initial import.

---
+++ trunk/hatch-common/src/main/java/org/linuxstuff/hatch/TimerStack.java
@@ -0,0 +1,30 @@
+package org.linuxstuff.hatch;
+
+import org.linuxstuff.hatch.logger.MetricsLoggerStrategy;
+
+/**
+ * The necessary methods to implement a timer stack. This is used by the live
+ * implementation in the normal use of Hatch, and in the null-pattern version of
+ * <b>hatch-null</b>. See TimerStackUtil in <b>hatch-core</b> for usage
+ * information of each method.
+ * 
+ * @author idcmp
+ * 
+ */
+interface TimerStack {
+
+	public void enableAndPush(final String key);
+
+	public void pop(final String key);
+
+	public void pop(final String key, final Long forcedDuration);
+
+	public void push(final String key);
+
+	public void setMetricsLogger(MetricsLoggerStrategy logger);
+
+	public void setState(TraceState state);
+
+	public void setThreadLocalMetricsLogger(MetricsLoggerStrategy logger);
+
+}
