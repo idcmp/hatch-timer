6a52cacec8deb84cc3e7288af05932c5227d6412
mr.idcmp <mr.idcmp@8ebe8d7f-f847-0410-ac63-8f6eca9f32e0>
2008-03-14 22:53:09 +0000

    [maven-release-plugin]  copy for tag 0.4.1

---
+++ tags/0.4.1/hatch-core/src/main/java/org/linuxstuff/hatch/logger/NullMetricsLogger.java
@@ -0,0 +1,18 @@
+package org.linuxstuff.hatch.logger;
+
+import org.linuxstuff.hatch.DurationBean;
+
+public class NullMetricsLogger implements MetricsLoggerStrategy {
+
+	public static final NullMetricsLogger LOGGER = new NullMetricsLogger();
+
+	public void error(String message) {
+		// do nothing!
+
+	}
+
+	public void logMetrics(DurationBean durationBean) {
+		// also do nothing!
+	}
+
+}
