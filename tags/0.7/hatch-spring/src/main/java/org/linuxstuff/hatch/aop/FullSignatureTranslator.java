b00f6e4fc65a4ca18f4cca220e4a012ab90ff0de
mr.idcmp <mr.idcmp@8ebe8d7f-f847-0410-ac63-8f6eca9f32e0>
2008-04-04 03:03:20 +0000

    [maven-release-plugin]  copy for tag 0.7

---
+++ tags/0.7/hatch-spring/src/main/java/org/linuxstuff/hatch/aop/FullSignatureTranslator.java
@@ -0,0 +1,14 @@
+package org.linuxstuff.hatch.aop;
+
+import org.aspectj.lang.Signature;
+
+/**
+ * Flattens signatures to a complete signature string. This calls
+ * {@link Signature#toLongString()} to determine the signature.
+ */
+public class FullSignatureTranslator implements SignatureTranslator {
+
+	public String nameCall(Signature signature) {
+		return signature.toLongString();
+	}
+}
